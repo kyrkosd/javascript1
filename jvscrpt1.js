0fb8db7fafa1519138b6b3a4cd62021682e62682
DK <kyrkos.d00@gmail.com>
2026-01-20 16:19:05 +0200

    Update jvscrpt1.js

--- jvscrpt1.js
+++ jvscrpt1.js
@@ -2,7 +2,7 @@ const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
+  return choices[randomIndex];  
 }
 
 function getPlayerChoice() {
